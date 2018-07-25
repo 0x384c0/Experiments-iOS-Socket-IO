50d9022f4ca01899a4a0d4b55b2fb0b050ada5cc
Andrew <andrew.ashurov@spalmalo.com>
2018-07-25 16:24:22 +0600

    Initial Commit

---
+++ Experiments-Socket-IO/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Experiments-Socket-IO
+//
+//  Created by Andrew Ashurov on 7/25/18.
+//  Copyright © 2018 0x384c0. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
