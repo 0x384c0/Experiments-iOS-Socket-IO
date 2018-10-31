11d0f4b836915e95355b9bb1fb7252c31d92e8b7
0x384c0 <0x384c0@gmail.com>
2018-10-31 17:38:53 +0600

    added SocketIO

--- Experiments-Socket-IO/ViewController.swift
+++ Experiments-Socket-IO/ViewController.swift
@@ -7,19 +7,53 @@
 //
 
 import UIKit
+import SocketIO
 
 class ViewController: UIViewController {
-
+    var socket:SocketIOClient!
+    var manager:SocketManager!
     override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        let params = ["token":"TOKEN",
+                      "toUser":"37"]
+        manager = SocketManager(socketURL: URL(string: "ws://localhost:3001")!,
+                                config: [.connectParams(params)])
+        
+        socket = manager.defaultSocket
+        
+        
+        
+        socket.on(clientEvent: .connect) {_,_  in
+            print("socket connected")
+        }
+        socket.on(clientEvent: .disconnect) { _,_ in
+            print("socket connected")
+        }
+        socket.on(clientEvent: .error) { _,_ in
+            print("socket error")
+        }
+        socket.on(clientEvent: .ping) { _,_ in
+            print("socket ping")
+        }
+        socket.on(clientEvent: .pong) { _,_ in
+            print("socket pong")
+        }
+        socket.on("new_message") {data, ack in
+            print("socket new_message")
+            print((data.first as! Dictionary<String,AnyObject>)["from"]!)
+            print((data.first as! Dictionary<String,AnyObject>)["message"]!)
+            
+        }
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    @IBAction func connect(_ sender: Any) {
+        socket.connect()
     }
-
-
+    @IBAction func diconnect(_ sender: Any) {
+        socket.disconnect()
+    }
+    @IBAction func sendTestMessage(_ sender: Any) {
+        print(socket.status)
+        socket.emit("send_message", ["message": "Я отправляю сообщение и это сообщение"])
+    }
 }
 
